0458b96ba7cf8826cfc6e84320afc4885a0d448b
gaogzhen <gaogzhen@foxmail.com>
2023-09-26 17:45:55 +0800

    init

---
+++ src/main/scala/com/gaogzhen/bigdata/chapter01/Hello.scala
@@ -0,0 +1,13 @@
+package com.gaogzhen.bigdata.chapter01
+
+/**
+ *
+ * @author gaogzhen
+ * @since 2023/9/26 13:53 
+ */
+object Hello {
+  def main(args: Array[String]): Unit = {
+    println("hello scala")
+    System.out.println("hello scala")
+  }
+}
